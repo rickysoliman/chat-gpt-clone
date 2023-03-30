84be632a0da9b9c4337999a8c7ce5b73cf6901c3
Ricky Marasigan <rickysoliman@gmail.com>
2023-03-30 15:48:22 -0400

    created basic input box and can grab user input plus some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'chat-gpt-clone';
+  userInput = '';
+
+  onSubmit(form: NgForm) {
+    this.userInput = form.value.userInput;
+    console.log(this.userInput);
+  }
 }
